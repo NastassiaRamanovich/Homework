e5529a9d60a3bc509b2626ebe12335ca8e2167ec
NastassiaRamanovich <nastassia_ramanovich@mail.ru>
2023-08-07 21:42:27 +0300

    Нахождение max и min из 2 чисел

--- Sem1_task2/Program.cs
+++ Sem1_task2/Program.cs
@@ -3,11 +3,11 @@ int a = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите b");
 int b= Convert.ToInt32(Console.ReadLine());
 
-if (a > b)
-{
-    Console.WriteLine("max = " + a);
-}
+if (a > b) 
+    {
+        Console.WriteLine("max = " + a);
+    }
 else
-{
-    Console.WriteLine("max = " + b);
-}
+    {
+        Console.WriteLine("max = " + b);
+    }
