952d6925618a32c3770ff82ba7e6dff7a4936830
NastassiaRamanovich <nastassia_ramanovich@mail.ru>
2023-08-07 21:30:26 +0300

    Вывод всех четных чисел от 1 до n

---
+++ Sem1_task8/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int x = 1;
+while (x <= n) 
+{
+    if ( x % 2 == 0)
+    {
+        Console.Write(x + ", ");
+    }
+    x++;
+}
